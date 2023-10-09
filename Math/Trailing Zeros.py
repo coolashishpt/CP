2d7f7f99da885261da5600481cd0c9b8bdaf1d53
coolashishpt <prasadashish24@gmail.com>
2023-10-10 01:09:01 +0530

    +++Add timer

--- Math/Trailing Zeros.py
+++ Math/Trailing Zeros.py
@@ -1,10 +1,13 @@
 # Naive Solution
-def trailingZeros(nums):
 
+from datetime import datetime
+
+# record current timestamp
+def trailingZeros(nums):
+    start = datetime.now()
     res = 1
     for num in range(2, nums+1):
         res *= num
-    print(res)
     ans = 0
     while res > 0:
         if res % 10 == 0:
@@ -13,18 +16,25 @@ def trailingZeros(nums):
             break
 
         res = res // 10
-
+    end = datetime.now()
+    td = (end - start).total_seconds() * 10**3
+    print(f"The time of execution of above program is : {td:.03f}ms")
     return ans
 
 # Optimize Solution
 
 def trailingZerosOptimizeV1(nums):
+    start = datetime.now()
     res = 0
     while nums >= 5:
         nums //= 5
         res += nums
-
+    end = datetime.now()
+    td = (end - start).total_seconds() * 10**3
+    print(f"The time of execution of above program is : {td:.03f}ms")
     return res
-print(trailingZeros(50))
 
-print(trailingZerosOptimizeV1(50))
+num = 22510
+print(trailingZeros(num))
+
+print(trailingZerosOptimizeV1(num))
