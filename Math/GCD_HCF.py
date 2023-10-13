dbddbc7a0742015bc4bab51f24ac81ca5514e533
coolashishpt <prasadashish24@gmail.com>
2023-10-14 01:06:33 +0530

    Did code linting

--- Math/GCD_HCF.py
+++ Math/GCD_HCF.py
@@ -1,30 +1,45 @@
-class GCD():
-    def gcd(numOne, numTwo):
+class GCD:
+    def __init__(self, numOne, numTwo):
+        self.numOne = numOne
+        self.numTwo = numTwo
 
-        if numOne > numTwo:
-            numOne, numTwo = numTwo, numOne
+    def gcd(self):
 
-        if numTwo % numOne == 0:
-            return numOne
+        if self.numOne > self.numTwo:
+            self.numOne, self.numTwo = self.numTwo, self.numOne
+
+        if self.numTwo % self.numOne == 0:
+            return self.numOne
 
         ans = 1
-        for num in range(1, numOne):
-            if numTwo % num == 0 and numOne % num == 0:
+        for num in range(1, self.numOne):
+            if self.numTwo % num == 0 and self.numOne % num == 0:
                 if num > ans:
                     ans = num
 
         return ans
 
+    def optimizedGCD(self):
+        """
+        # optimized way to find GCD
 
-    def optimizedGCD(numOne, numTwo):
-        if numTwo == 0:
-            return numOne
+        def func(n1, n2):
+            if n2 == 0:
+                return n1
 
-        return GCD.optimizedGCD(numTwo, numOne%numTwo)
+            return func(n2, n1 % n2)
+
+        """
+        if self.numTwo == 0:
+            return self.numOne
+
+        Recursive = GCD(self.numTwo, self.numOne % self.numTwo)
+        return Recursive.optimizedGCD()
 
 
 if __name__ == "__main__":
     a = 27
     b = 15
-    print(GCD.gcd(a, b))
-    print(GCD.optimizedGCD(a, b))
+    G = GCD(a, b)
+    print(G.gcd())
+    print(G.optimizedGCD())
--- Math/LCM.py
+++ Math/LCM.py
@@ -1,7 +1,8 @@
 from GCD_HCF import GCD
 
+
 def LCM(numOne, numTwo):
 
-    return (numOne * numTwo) // GCD.optimizedGCD(numOne, numTwo)
+    return (numOne * numTwo) // GCD(numOne, numTwo).optimizedGCD()
 
 print(LCM(4, 6))
